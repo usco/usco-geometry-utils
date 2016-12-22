c33cb76c010f27407239b48948c97a7839f888ad
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-22 11:25:09 +0100

    feat(svgStringAsGeo): updated api , added passable options & defaults

--- src/svgStringAsGeometry.js
+++ src/svgStringAsGeometry.js
@@ -1,11 +1,14 @@
 import svgMesh3d from 'svg-mesh-3d'
 var parsePath = require('extract-svg-path').parse
 
-export default function svgStringAsGeometry (svgString) {
-  const svgPath = parsePath(svgString)
-  const logoMesh = svgMesh3d(svgPath, {
+export default function svgStringAsGeometry (svgString, options) {
+  const defaults = {
     delaunay: true,
-    scale: 1
-  })
+    scale: 1,
+    clean: true
+  }
+  options = Object.assign({}, defaults, options)
+  const svgPath = parsePath(svgString)
+  const logoMesh = svgMesh3d(svgPath, options)
   return logoMesh
 }
